6e4a46a06e92ee84521bb45350f364f8d98a7609
Ubuntu <ubuntu@ip-172-31-16-231.eu-west-1.compute.internal>
2018-02-06 09:34:26 +0000

    Add main.py

--- systeminfo/main.py
+++ systeminfo/main.py
@@ -4,9 +4,13 @@ Created on 25 Jan 2018
 @author: aoifeosullivan
 '''
 import platform
+import shutil
 
 def main():
-    print(platform.platform())
-    return
+    print("The platform is", platform.platform())
+    total, used, free = shutil.disk_usage(__file__)
+    print ("total, used, free: ", total, used, free)	
+	return
 if __name__ == '__main__':
     main()
+
